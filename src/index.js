8c6cd2bce4cfb4b80dbb6b0ba088e88f7d30d454
Eva-Rae McLean <evarae.mclean@gmail.com>
2022-01-16 22:35:07 +1300

    browserRouter
    browserRouter

--- src/index.js
+++ src/index.js
@@ -8,16 +8,16 @@ import { BrowserRouter as Router, Routes, Route, Link, Navigate} from 'react-rou
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <div>
-        <Routes>
-          <Route path='/' element={<Navigate to = '/landing'/>}/>
-          <Route path='/landing' element={<Landing/>}/>
-          <Route path='/home' element={<Home/>}/>
-          <Route path='*' element={<p>404 Not Found!!</p>}/>
-        </Routes>
-      </div>
-    </Router>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <div>
+          <Routes>
+            <Route path='/' element={<Navigate to = '/landing'/>}/>
+            <Route path='/landing' element={<Landing/>}/>
+            <Route path='/home' element={<Home/>}/>
+            <Route path='*' element={<p>404 Not Found!!</p>}/>
+          </Routes>
+        </div>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
